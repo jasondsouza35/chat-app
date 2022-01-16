598d34c586b6d4047f177176212f4131d96385de
Jason D'Souza <jasonaldrindsouza@gmail.com>
2022-01-16 14:54:40 -0500

    Updates to code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from "react-chat-engine";
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height = "100vh"
+            projectID = "eec084d3-cbf8-44ca-9ceb-0eda12641611"
+            userName = "jason_dsouza_"
+            userSecret = "jason2003" 
+        />
+    );
+}
+
+export default App;
